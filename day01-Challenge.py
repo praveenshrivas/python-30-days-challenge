51ef7a6868a8c0486422ae1e06c4271ab46adddf
praveenshrivas <47313992+praveenshrivas@users.noreply.github.com>
2025-04-04 09:31:26 +0530

    Update day01-Challenge.py

--- day01-Challenge.py
+++ day01-Challenge.py
@@ -1,6 +1,17 @@
 """
-TASK: Take the input from the user whatever he wants to write and then the output will be like this:
+GOAL: 
+1. Read a line of text from the input.
+2. Print "Hello, World." on the first line.
+3. Print the input text on the second line.
 
+Example: 
+Input: Welcome to 30 days of code. 
+
+Output: 
 Hello, World. 
-Hello from Python.
+Welcome to 30 days of code. 
 """
+# code starts from here. 
+words = input()
+print("Hello, World.")
+print(words)
